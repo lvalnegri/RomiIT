1b7b4bf388d0fab6403a6a6a98c3f3f6d4df384c
Luca Valnegri <l.valnegri@datamaps.co.uk>
2022-09-12 00:28:44 +0100

    first commit

---
+++ R/RomiIT.R
@@ -0,0 +1,11 @@
+#' RomiIT
+#' 
+#' Data, geographical boundaries and functionalities to facilitate analysis about `OMI` areas for Italy.
+#' 
+#' `OMI` stands for *Osservatorio del Mercato Immobiliare*, part of the *Agenzia delle Entrate* (the Italian *Inland Revenue* Office),
+#' whose job is the collection and deployment of data related to the general housing and commercial private market.
+#' 
+#'  
+#' @docType package
+#' @name RomiIT
+NULL
